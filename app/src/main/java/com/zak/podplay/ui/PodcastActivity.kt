008e3d8c3cb7a0fd6f95978bd7d592f47ddc0aac
learnWithZak <learn.with.zak@gmail.com>
2022-11-02 04:58:17 +0100

    refactor: Creating the podcast response model

--- app/src/main/java/com/zak/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/zak/podplay/ui/PodcastActivity.kt
@@ -2,11 +2,27 @@ package com.zak.podplay.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.zak.podplay.R
+import com.zak.podplay.repository.ItunesRepo
+import com.zak.podplay.service.ItunesService
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class PodcastActivity : AppCompatActivity() {
+
+    val TAG = javaClass.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcast)
+
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo(itunesService)
+
+        GlobalScope.launch {
+            val results = itunesRepo.searchByTerm("Android Developer")
+            Log.i(TAG, "Results = ${results.body()}")
+        }
     }
 }
