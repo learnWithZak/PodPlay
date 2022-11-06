a6a58872cd973f52663c224a3fe2e983f075eeb7
learnWithZak <learn.with.zak@gmail.com>
2022-11-06 04:05:22 +0100

    refactor: Loading the options menu

--- app/src/main/java/com/zak/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/zak/podplay/ui/PodcastActivity.kt
@@ -1,8 +1,11 @@
 package com.zak.podplay.ui
 
+import android.app.SearchManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import com.zak.podplay.R
 import com.zak.podplay.repository.ItunesRepo
 import com.zak.podplay.service.ItunesService
@@ -24,5 +27,16 @@ class PodcastActivity : AppCompatActivity() {
             val results = itunesRepo.searchByTerm("Android Developer")
             Log.i(TAG, "Results = ${results.body()}")
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+        val searchMenuItem = menu?.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+        val searchManager = getSystemService(SEARCH_SERVICE) as SearchManager
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        return true
+
     }
 }
