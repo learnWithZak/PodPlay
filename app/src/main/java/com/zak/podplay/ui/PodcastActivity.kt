2084fd134fecc5994e12965616340c7b55e28097
learnWithZak <learn.with.zak@gmail.com>
2022-11-09 05:04:43 +0100

    refactor: Populating the RecyclerView

--- app/src/main/java/com/zak/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/zak/podplay/ui/PodcastActivity.kt
@@ -6,24 +6,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.zak.podplay.R
+import com.zak.podplay.adapter.PodcastListAdapter
 import com.zak.podplay.databinding.ActivityPodcastBinding
 import com.zak.podplay.repository.ItunesRepo
 import com.zak.podplay.service.ItunesService
+import com.zak.podplay.viewmodel.SearchViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class PodcastActivity : AppCompatActivity() {
+class PodcastActivity : AppCompatActivity(), PodcastListAdapter.PodcastListAdapterListener {
 
     val TAG = javaClass.simpleName
-    private lateinit var binding: ActivityPodcastBinding
+    private lateinit var databinding: ActivityPodcastBinding
+    private val searchViewModel by viewModels<SearchViewModel>()
+    private lateinit var podcastListAdapter: PodcastListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPodcastBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        databinding = ActivityPodcastBinding.inflate(layoutInflater)
+        setContentView(databinding.root)
         setupToolbar()
+        setupViewModels()
+        updateControls()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -50,17 +62,49 @@ class PodcastActivity : AppCompatActivity() {
     }
 
     private fun performSearch(term: String) {
-        val itunesService = ItunesService.instance
-        val itunesRepo = ItunesRepo(itunesService)
-
+        showProgressBar()
         GlobalScope.launch {
-            val results = itunesRepo.searchByTerm(term)
-            Log.i(TAG, "Results = ${results.body()}")
+            val results = searchViewModel.searchPodcasts(term)
+            withContext(Dispatchers.Main) {
+                hideProgressBar()
+                databinding.toolbar.title = term
+                podcastListAdapter.setSearchData(results)
+            }
         }
     }
 
     private fun setupToolbar() {
-        setSupportActionBar(binding.toolbar)
+        setSupportActionBar(databinding.toolbar)
     }
 
+    private fun setupViewModels() {
+        val service = ItunesService.instance
+        searchViewModel.itunesRepo = ItunesRepo(service)
+    }
+
+    private fun updateControls() {
+        databinding.podcastRecyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(this)
+        databinding.podcastRecyclerView.layoutManager = layoutManager
+
+        val dividerItemDecoration = DividerItemDecoration(databinding.podcastRecyclerView.context, layoutManager.orientation)
+
+        databinding.podcastRecyclerView.addItemDecoration(dividerItemDecoration)
+
+        podcastListAdapter = PodcastListAdapter(null, this, this)
+        databinding.podcastRecyclerView.adapter = podcastListAdapter
+    }
+
+    override fun onShowDetails(podcastSummaryViewData: SearchViewModel.PodcastSummaryViewData) {
+        TODO("Not yet implemented")
+    }
+
+    private fun showProgressBar() {
+        databinding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        databinding.progressBar.visibility = View.INVISIBLE
+    }
 }
