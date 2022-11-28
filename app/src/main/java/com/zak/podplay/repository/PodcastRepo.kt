be68db397e81cea4d49d29ba920791ef92be2d05
learnWithZak <learn.with.zak@gmail.com>
2022-11-28 11:22:20 +0100

    refactor: RSS feed of podcast episodes

--- app/src/main/java/com/zak/podplay/repository/PodcastRepo.kt
+++ app/src/main/java/com/zak/podplay/repository/PodcastRepo.kt
@@ -1,10 +1,18 @@
 package com.zak.podplay.repository
 
 import com.zak.podplay.model.Podcast
+import com.zak.podplay.service.RssFeedService
+import kotlinx.coroutines.launch
+import javax.xml.bind.JAXBElement.GlobalScope
 
 class PodcastRepo {
 
     fun getPodcast(feedUrl: String): Podcast? {
+        val rssFeedService = RssFeedService.instance
+        kotlinx.coroutines.GlobalScope.launch {
+            rssFeedService.getFeed(feedUrl)
+        }
+
         return Podcast(
             feedUrl = feedUrl,
             feedTitle = "No name",
---
+++ app/src/main/java/com/zak/podplay/service/RssFeedService.kt
@@ -0,0 +1,73 @@
+package com.zak.podplay.service
+
+
+import com.zak.podplay.BuildConfig
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import okhttp3.logging.HttpLoggingInterceptor
+import org.w3c.dom.Node
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Url
+import java.util.concurrent.TimeUnit
+import javax.xml.parsers.DocumentBuilderFactory
+
+/**
+ * RssFeedService to process RSS feed
+ */
+class RssFeedService private constructor() {
+
+    suspend fun getFeed(xmlFileURL: String): RssFeedResponse? {
+        val service: FeedService
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val client = OkHttpClient().newBuilder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+
+        if (BuildConfig.DEBUG) {
+            client.addInterceptor(interceptor)
+        }
+        client.build()
+
+        val retrofit = Retrofit.Builder()
+            // change from: https://atp.fm/episodes?format=rss to: https://atp.fm/episodes/.
+            .baseUrl("${xmlFileURL.split("?")[0]}/")
+            .build()
+        service = retrofit.create(FeedService::class.java)
+        try {
+            val result = service.getFeed(xmlFileURL)
+            return if (result.code() >= 400) {
+                println("server error, ${result.code()}, ${result.errorBody()}")
+                null
+            } else {
+                val rssFeedResponse: RssFeedResponse? = null
+                println(result.body()?.string())
+                rssFeedResponse
+            }
+        } catch (t: Throwable) {
+            println("error, ${t.localizedMessage}")
+        }
+        return null
+    }
+
+    companion object {
+        val instance: RssFeedService by lazy {
+            RssFeedService()
+        }
+    }
+}
+
+interface FeedService {
+    @Headers(
+        "Content-Type: application/xml; charset=utf-8",
+        "Accept: application/xml"
+    )
+    @GET
+    suspend fun getFeed(@Url xmlFileURL: String): Response<ResponseBody>
+}
