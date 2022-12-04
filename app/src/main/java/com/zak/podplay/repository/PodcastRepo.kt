990ec7c6ff8eb290b921af2e6f3d2a0ffe55cb1a
learnWithZak <learn.with.zak@gmail.com>
2022-12-04 02:27:29 +0100

    refactor: Updating the podcast repo

--- app/src/main/java/com/zak/podplay/repository/PodcastRepo.kt
+++ app/src/main/java/com/zak/podplay/repository/PodcastRepo.kt
@@ -1,23 +1,43 @@
 package com.zak.podplay.repository
 
+import com.zak.podplay.model.Episode
 import com.zak.podplay.model.Podcast
+import com.zak.podplay.service.RssFeedResponse
 import com.zak.podplay.service.RssFeedService
-import kotlinx.coroutines.launch
-import javax.xml.bind.JAXBElement.GlobalScope
+import com.zak.podplay.util.DateUtils.xmlDateToDate
 
-class PodcastRepo {
+class PodcastRepo(private val feedService: RssFeedService) {
 
-    fun getPodcast(feedUrl: String): Podcast? {
-        val rssFeedService = RssFeedService.instance
-        kotlinx.coroutines.GlobalScope.launch {
-            rssFeedService.getFeed(feedUrl)
+    suspend fun getPodcast(feedUrl: String): Podcast? {
+        var podcast: Podcast? = null
+        val feedResponse = feedService.getFeed(feedUrl)
+        if (feedResponse != null) {
+            podcast = rssResponseToPodcast(feedUrl, "", feedResponse)
         }
+        return podcast
+    }
 
-        return Podcast(
-            feedUrl = feedUrl,
-            feedTitle = "No name",
-            feedDesc = "No Description",
-            imageUrl = "No image"
-        )
+    private fun rssItemsToEpisodes(
+        episodeResponses: List<RssFeedResponse.EpisodeResponse>
+    ): List<Episode> {
+        return episodeResponses.map {
+            Episode(
+                it.guid ?: "",
+                it.title ?: "",
+                it.description ?: "",
+                it.url ?: "",
+                it.type ?: "",
+                xmlDateToDate(it.pubDate),
+                it.duration ?: ""
+            )
+        }
+    }
+
+    private fun rssResponseToPodcast(
+        feedUrl: String, imageUrl: String, rssResponse: RssFeedResponse
+    ): Podcast? {
+        val items = rssResponse.episodes ?: return null
+        val description = if (rssResponse.description == "") rssResponse.summary else rssResponse.description
+        return Podcast(feedUrl, rssResponse.title, description, imageUrl, rssResponse.lastUpdated, episodes = rssItemsToEpisodes(items))
     }
 }
