c8b77e2db026b2ed0fb711aa0ddde52078f3c198
learnWithZak <learn.with.zak@gmail.com>
2022-12-06 05:55:50 +0100

    refactor: Room type converters

--- app/src/main/java/com/zak/podplay/db/PodPlayDatabase.kt
+++ app/src/main/java/com/zak/podplay/db/PodPlayDatabase.kt
@@ -4,11 +4,28 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import com.zak.podplay.model.Episode
 import com.zak.podplay.model.Podcast
 import kotlinx.coroutines.CoroutineScope
+import java.util.*
+
+class Converters {
+
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return if (value == null) null else Date(value)
+    }
+
+    @TypeConverter
+    fun toTimestamp(date: Date?): Long? {
+        return (date?.time)
+    }
+}
 
 @Database(entities = [Podcast::class, Episode::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class PodPlayDatabase: RoomDatabase() {
 
     abstract fun podcastDao(): PodcastDao
