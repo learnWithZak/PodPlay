792695a4e1ea86ecffe5d2d4990e2e6e49f4fe55
learnWithZak <learn.with.zak@gmail.com>
2022-12-17 04:53:01 +0100

    refactor: Building the MediaBrowserService

---
+++ app/src/main/java/com/zak/podplay/service/PodPlayMediaService.kt
@@ -0,0 +1,27 @@
+package com.zak.podplay.service
+
+import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import androidx.media.MediaBrowserServiceCompat
+
+class PodPlayMediaService: MediaBrowserServiceCompat() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onGetRoot(
+        clientPackageName: String,
+        clientUid: Int,
+        rootHints: Bundle?
+    ): BrowserRoot? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoadChildren(
+        parentId: String,
+        result: Result<MutableList<MediaBrowserCompat.MediaItem>>
+    ) {
+        TODO("Not yet implemented")
+    }
+}
