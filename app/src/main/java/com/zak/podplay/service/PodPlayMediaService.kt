1bec98e547c8ad7f652f99635ac72519d55999b8
learnWithZak <learn.with.zak@gmail.com>
2022-12-17 05:15:58 +0100

    refactor: Create a MediaSession

--- app/src/main/java/com/zak/podplay/service/PodPlayMediaService.kt
+++ app/src/main/java/com/zak/podplay/service/PodPlayMediaService.kt
@@ -2,12 +2,16 @@ package com.zak.podplay.service
 
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
 import androidx.media.MediaBrowserServiceCompat
 
 class PodPlayMediaService: MediaBrowserServiceCompat() {
 
+    private lateinit var mediaSession: MediaSessionCompat
+
     override fun onCreate() {
         super.onCreate()
+        createMediaSession()
     }
 
     override fun onGetRoot(
@@ -23,5 +27,12 @@ class PodPlayMediaService: MediaBrowserServiceCompat() {
         result: Result<MutableList<MediaBrowserCompat.MediaItem>>
     ) {
         TODO("Not yet implemented")
+    }
+
+    private fun createMediaSession() {
+        mediaSession = MediaSessionCompat(this, "PodPlayMediaService")
+        sessionToken = mediaSession.sessionToken
+        val callback = PodPlayMediaCallback(this, mediaSession)
+        mediaSession.setCallback(callback)
     }
 }
