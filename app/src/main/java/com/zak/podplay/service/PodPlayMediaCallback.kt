fb5b3c8f7297aa181757c0f2e9ac7a4b7415ba23
learnWithZak <learn.with.zak@gmail.com>
2022-12-19 05:04:03 +0100

    refactor: Updating media session state

--- app/src/main/java/com/zak/podplay/service/PodPlayMediaCallback.kt
+++ app/src/main/java/com/zak/podplay/service/PodPlayMediaCallback.kt
@@ -1,10 +1,13 @@
 package com.zak.podplay.service
 
 import android.content.Context
+import android.media.MediaMetadata
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
+import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
 
 class PodPlayMediaCallback(
     val context: Context,
@@ -16,11 +19,19 @@ class PodPlayMediaCallback(
         super.onPlayFromUri(uri, extras)
         println("Playing ${uri.toString()}")
         onPlay()
+        mediaSession.setMetadata(
+            MediaMetadataCompat.Builder()
+                .putString(
+                    MediaMetadataCompat.METADATA_KEY_MEDIA_URI,
+                    uri.toString()
+                ).build()
+        )
     }
 
     override fun onPlay() {
         super.onPlay()
         println("onPlay called")
+        setState(PlaybackStateCompat.STATE_PLAYING)
     }
 
     override fun onStop() {
@@ -31,5 +42,21 @@ class PodPlayMediaCallback(
     override fun onPause() {
         super.onPause()
         println("onPause called")
+        setState(PlaybackStateCompat.STATE_PAUSED)
+    }
+
+    private fun setState(state: Int) {
+        var position: Long = -1
+
+        val playbackState = PlaybackStateCompat.Builder()
+            .setActions(
+                PlaybackStateCompat.ACTION_PLAY or
+                        PlaybackStateCompat.ACTION_STOP or
+                        PlaybackStateCompat.ACTION_PLAY_PAUSE or
+                        PlaybackStateCompat.ACTION_PAUSE)
+            .setState(state, position, 1.0f)
+            .build()
+
+        mediaSession.setPlaybackState(playbackState)
     }
 }
