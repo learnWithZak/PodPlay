85d87f98bfb9dd7dbf2938a4f6b5512bd635d6c1
learnWithZak <learn.with.zak@gmail.com>
2022-12-21 05:01:46 +0100

    refactor: Episode player details

--- app/src/main/java/com/zak/podplay/ui/EpisodePlayerFragment.kt
+++ app/src/main/java/com/zak/podplay/ui/EpisodePlayerFragment.kt
@@ -1,15 +1,22 @@
 package com.zak.podplay.ui
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
 import com.zak.podplay.databinding.FragmentEpisodePlayerBinding
+import com.zak.podplay.util.HtmlUtils
+import com.zak.podplay.viewmodel.PodcastViewModel
 
 class EpisodePlayerFragment : Fragment() {
 
     private lateinit var databinding: FragmentEpisodePlayerBinding
+    private val podcastViewModel: PodcastViewModel by activityViewModels()
 
     companion object {
         fun newInstance(): EpisodePlayerFragment {
@@ -30,6 +37,7 @@ class EpisodePlayerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        updateControls()
     }
 
     override fun onStart() {
@@ -38,5 +46,17 @@ class EpisodePlayerFragment : Fragment() {
 
     override fun onStop() {
         super.onStop()
+    }
+
+    private fun updateControls() {
+        databinding.episodeTitleTextView.text = podcastViewModel.activeEpisodeViewData?.title
+        val htmlDesc = podcastViewModel.activeEpisodeViewData?.description ?: ""
+        val descSpan = HtmlUtils.htmlToSpannable(htmlDesc)
+        databinding.episodeDescTextView.text = descSpan
+        databinding.episodeDescTextView.movementMethod = ScrollingMovementMethod()
+        val fragmentActivity = activity as FragmentActivity
+        Glide.with(fragmentActivity)
+            .load(podcastViewModel.podcastLiveData.value?.imageUrl)
+            .into(databinding.episodeImageView)
     }
 }
